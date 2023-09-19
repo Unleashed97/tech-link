42fd729147dc3ae4f633d85670ef77cd6839d834
Roman <noudev@yandex.ru>
2023-09-19 17:47:49 +0300

    feat: add header

---
+++ src/js/script.js
@@ -0,0 +1,6 @@
+import * as customFunctions from './modules/functions.js'
+
+customFunctions.handleBurger()
+customFunctions.handleModal()
+customFunctions.handleAccordion()
+customFunctions.handleTabs()
